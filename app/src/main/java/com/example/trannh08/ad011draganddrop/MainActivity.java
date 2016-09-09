76a3fa6ca7c09d0c8ae73db4f25f589be24ea0d0
Koichi <koichisojiro@live.com>
2016-09-09 21:47:20 +0700

    code works

--- app/src/main/java/com/example/trannh08/ad011draganddrop/MainActivity.java
+++ app/src/main/java/com/example/trannh08/ad011draganddrop/MainActivity.java
@@ -1,155 +1,59 @@
 package com.example.trannh08.ad011draganddrop;
 
-import android.content.ClipData;
-import android.content.ClipDescription;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imageView;
-    String message;
-    private android.widget.RelativeLayout.LayoutParams layoutParams;
+    private ImageView imageView;
+    private ViewGroup rootLayout;
+    private int _xDelta;
+    private int _yDelta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = (ImageView) findViewById(R.id.imageView);
-        imageView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                ClipData.Item item = new ClipData.Item((CharSequence) view.getTag());
-                String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+        rootLayout = (ViewGroup) findViewById(R.id.view_root);
+        imageView = (ImageView) rootLayout.findViewById(R.id.imageView);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 150);
+        imageView.setLayoutParams(layoutParams);
+        imageView.setOnTouchListener(new ChoiceTouchListener());
+    }
 
-                ClipData clipData = new ClipData(view.getTag().toString(), mimeTypes, item);
-                View.DragShadowBuilder myShadow = new View.DragShadowBuilder(imageView);
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                    view.startDragAndDrop(clipData, myShadow, null, 0);
-                } else {
-                    view.startDrag(clipData, myShadow, null, 0);
-                }
-                return true;
+    private final class ChoiceTouchListener implements View.OnTouchListener {
+        public boolean onTouch(View view, MotionEvent event) {
+            final int X = (int) event.getRawX();
+            final int Y = (int) event.getRawY();
+            switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_DOWN:
+                    RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                    _xDelta = X - lParams.leftMargin;
+                    _yDelta = Y - lParams.topMargin;
+                    break;
+                case MotionEvent.ACTION_UP:
+                    break;
+                case MotionEvent.ACTION_POINTER_DOWN:
+                    break;
+                case MotionEvent.ACTION_POINTER_UP:
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
+                            .getLayoutParams();
+                    layoutParams.leftMargin = X - _xDelta;
+                    layoutParams.topMargin = Y - _yDelta;
+                    layoutParams.rightMargin = -250;
+                    layoutParams.bottomMargin = -250;
+                    view.setLayoutParams(layoutParams);
+                    break;
             }
-        });
-
-        imageView.setOnDragListener(new View.OnDragListener() {
-            @Override
-            public boolean onDrag(View view, DragEvent event) {
-                switch (event.getAction()) {
-                    case DragEvent.ACTION_DRAG_STARTED:
-                        layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-                        Log.d(message, "Action is DragEvent.ACTION_DRAG_STARTED");
-                        // Do nothing
-                        break;
-
-                    case DragEvent.ACTION_DRAG_ENTERED:
-                        Log.d(message, "Action is DragEvent.ACTION_DRAG_ENTERED");
-                        int x_cord = (int) event.getX();
-                        int y_cord = (int) event.getY();
-                        break;
-
-                    case DragEvent.ACTION_DRAG_EXITED:
-                        Log.d(message, "Action is DragEvent.ACTION_DRAG_EXITED");
-                        x_cord = (int) event.getX();
-                        y_cord = (int) event.getY();
-                        layoutParams.leftMargin = x_cord;
-                        layoutParams.topMargin = y_cord;
-                        view.setLayoutParams(layoutParams);
-                        break;
-
-                    case DragEvent.ACTION_DRAG_LOCATION:
-                        Log.d(message, "Action is DragEvent.ACTION_DRAG_LOCATION");
-                        x_cord = (int) event.getX();
-                        y_cord = (int) event.getY();
-                        break;
-
-                    case DragEvent.ACTION_DRAG_ENDED:
-                        Log.d(message, "Action is DragEvent.ACTION_DRAG_ENDED");
-                        // Do nothing
-                        break;
-
-                    case DragEvent.ACTION_DROP:
-                        Log.d(message, "ACTION_DROP event");
-                        // Do nothing
-                        break;
-                    default:
-                        break;
-                }
-                return true;
-            }
-        });
-
-
-//            Drawable shape = getResources().getDrawable(R.drawable.ic_action_name);
-//            @Override
-//            public boolean onDrag(View view, DragEvent event) {
-//                int action = event.getAction();
-//                switch (action) {
-//                    case DragEvent.ACTION_DRAG_STARTED:
-//                        //do nothing
-//                        break;
-//                    case DragEvent.ACTION_DRAG_ENTERED:
-//                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-//                            view.setBackground(shape);
-//                        } else {
-//                            view.setBackgroundDrawable(shape);
-//                        }
-//                        break;
-//                    case DragEvent.ACTION_DRAG_EXITED:
-//                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-//                            view.setBackground(shape);
-//                        } else {
-//                            view.setBackgroundDrawable(shape);
-//                        }
-//                        break;
-//                    case DragEvent.ACTION_DROP:
-//                        View myView = (View) event.getLocalState();
-//                        ViewGroup owner = (ViewGroup) myView.getParent();
-//                        owner.removeView(myView);
-//                        RelativeLayout container = (RelativeLayout) myView;
-//                        container.addView(myView);
-//                        myView.setVisibility(View.VISIBLE);
-//                        break;
-//                    case DragEvent.ACTION_DRAG_ENDED:
-//                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-//                            view.setBackground(shape);
-//                        } else {
-//                            view.setBackgroundDrawable(shape);
-//                        }
-//                        default:
-//                        break;
-//                }
-//                return true;
-//            }
-//        });
-
-        imageView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    ClipData clipData = ClipData.newPlainText("", "");
-                    View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(imageView);
-
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                        imageView.startDragAndDrop(clipData, shadowBuilder, imageView, 0);
-                    } else {
-                        imageView.startDrag(clipData, shadowBuilder, imageView, 0);
-                    }
-                    imageView.setVisibility(View.INVISIBLE);
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        });
+            rootLayout.invalidate();
+            return true;
+        }
     }
 }
