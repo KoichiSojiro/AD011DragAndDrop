21e450dbd060c45491274d2c98089939eb8e4c7f
Koichi <koichisojiro@live.com>
2016-09-10 17:31:46 +0700

    complete

--- app/src/main/java/com/example/trannh08/ad011draganddrop/MainActivity.java
+++ app/src/main/java/com/example/trannh08/ad011draganddrop/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.trannh08.ad011draganddrop;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,10 +10,14 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "AD011";
+    private final int IMAGE_WIDTH = 100;
+    private final int IMAGE_HEIGHT = 100;
+
     private ImageView imageView;
     private ViewGroup rootLayout;
-    private int _xDelta;
-    private int _yDelta;
+    private int deltaX;
+    private int deltaY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,37 +26,56 @@ public class MainActivity extends AppCompatActivity {
 
         rootLayout = (ViewGroup) findViewById(R.id.view_root);
         imageView = (ImageView) rootLayout.findViewById(R.id.imageView);
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 150);
+
+        /** Important section:
+         * 1: define a Mask (the image)
+         * 2:  put the Mask on the item to make it become movable one
+         * 3: set OnTouchListener for the movable item to get the action and set destination for it
+         */
+        // 1: define the Mask
+        RelativeLayout.LayoutParams layoutParams =
+                new RelativeLayout.LayoutParams(IMAGE_WIDTH, IMAGE_HEIGHT);
+        // 2: put the Mask on it
         imageView.setLayoutParams(layoutParams);
+        // 3: set OnTouchListener
         imageView.setOnTouchListener(new ChoiceTouchListener());
     }
 
+    // declare custom class ChoiceTouchListener implements the OnTouchListener
     private final class ChoiceTouchListener implements View.OnTouchListener {
         public boolean onTouch(View view, MotionEvent event) {
-            final int X = (int) event.getRawX();
-            final int Y = (int) event.getRawY();
+            int rawX = (int) event.getRawX();
+            int rawY = (int) event.getRawY();
+            RelativeLayout.LayoutParams layoutParams;
+
             switch (event.getAction() & MotionEvent.ACTION_MASK) {
                 case MotionEvent.ACTION_DOWN:
-                    RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-                    _xDelta = X - lParams.leftMargin;
-                    _yDelta = Y - lParams.topMargin;
+                    Log.d(TAG, "MotionEvent.ACTION_DOWN");
+                    layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                    deltaX = rawX - layoutParams.leftMargin;
+                    deltaY = rawY - layoutParams.topMargin;
                     break;
                 case MotionEvent.ACTION_UP:
+                    Log.d(TAG, "MotionEvent.ACTION_UP");
                     break;
                 case MotionEvent.ACTION_POINTER_DOWN:
+                    Log.d(TAG, "MotionEvent.ACTION_POINTER_DOWN");
                     break;
                 case MotionEvent.ACTION_POINTER_UP:
+                    Log.d(TAG, "MotionEvent.ACTION_POINTER_UP");
                     break;
                 case MotionEvent.ACTION_MOVE:
-                    RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
-                            .getLayoutParams();
-                    layoutParams.leftMargin = X - _xDelta;
-                    layoutParams.topMargin = Y - _yDelta;
-                    layoutParams.rightMargin = -250;
-                    layoutParams.bottomMargin = -250;
+                    Log.d(TAG, "MotionEvent.ACTION_MOVE");
+                    layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                    layoutParams.leftMargin = rawX - deltaX;
+                    layoutParams.topMargin = rawY - deltaY;
+                    layoutParams.rightMargin = -IMAGE_HEIGHT;
+                    layoutParams.bottomMargin = -IMAGE_WIDTH;
                     view.setLayoutParams(layoutParams);
                     break;
             }
+
+            // refresh the View
             rootLayout.invalidate();
             return true;
         }
